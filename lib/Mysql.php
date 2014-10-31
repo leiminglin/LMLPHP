05e8cee0a3528c8aba80c95463f7263d61b27fe8
leiminglin <leiminglin@126.com>
2014-10-31 13:09:01 +0800

    add Mysql driver class,lib/Mysql.php;

---
+++ lib/Mysql.php
@@ -0,0 +1,169 @@
+<?php
+/**
+* LMLPHP Framework
+* Copyright (c) 2014 http://lmlphp.com All rights reserved.
+* Licensed ( http://mit-license.org/ )
+* Author: leiminglin <leiminglin@126.com>
+*
+* A fully object-oriented PHP framework.
+* Keep it light, magnificent, lovely.
+*
+*/
+
+class Mysql{
+	
+	private $link;
+	private $resource;
+	public static $instance;
+
+	private function __construct($config){
+		if ( !extension_loaded('mysql') ) {
+			throw new LmlDbException('MySQL support not be enabled');
+		}
+		$this->connect($config);
+	}
+	
+	public static function getInstance($config){
+		if(self::$instance){
+			return self::$instance;
+		}
+		return new self($config);
+	}
+
+	public function connect($config='') {
+		if ( !$this->link ) {
+			$host = $config['hostname'].($config['hostport']?":{$config['hostport']}":'');
+			$pconnect = isset($config['persist'])?$config['persist']:0;
+			if($pconnect){
+				$this->link = mysql_pconnect($host, $config['username'], $config['password'], 
+					MYSQL_CLIENT_COMPRESS*MYSQL_CLIENT_IGNORE_SPACE*
+						MYSQL_CLIENT_INTERACTIVE*MYSQL_CLIENT_SSL);
+			}else{
+				$this->link = mysql_connect($host, $config['username'], $config['password'], true, 
+					MYSQL_CLIENT_COMPRESS*MYSQL_CLIENT_IGNORE_SPACE*
+						MYSQL_CLIENT_INTERACTIVE*MYSQL_CLIENT_SSL);
+			}
+			if ( !$this->link || (!empty($config['database']) && 
+					!mysql_select_db($config['database'], $this->link) ) ) {
+				throw new LmlDbException(mysql_error());
+			}
+			$dbVersion = mysql_get_server_info($this->link);
+			mysql_query("SET NAMES '".$config['charset']."'", $this->link);
+			if($dbVersion >'5.0.1'){
+				mysql_query("SET sql_mode=''",$this->link);
+			}
+		}
+		return $this;
+	}
+
+	public function free() {
+		if( is_resource($this->resource) ){
+			mysql_free_result($this->resource);
+		}
+		$this->resource = null;
+	}
+
+	public function query($str) {
+		if($this->resource){
+			$this->free();
+		}
+		$this->resource = mysql_query($str, $this->link);
+		if( false === $this->resource) {
+			$this->error();
+			return false;
+		}elseif(is_resource($this->resource)) {
+			$this->numRows = mysql_num_rows($this->resource);
+			return $this->getAll();
+		}else{
+			return mysql_affected_rows($this->link);
+		}
+	}
+	
+	public function insert($table, $arr){
+		$sql = 'INSERT INTO '.$table;
+		foreach ($arr as $k=>$v){
+			$keys[] = '`'.$k.'`';
+			$vals[] = '\''.$this->escapeString($v).'\'';
+		}
+		$sql .= '('.implode(',', $keys).')';
+		$sql .= 'VALUES('.implode(',', $vals).')';
+		return $this->query($sql);
+	}
+	
+	public function getOne($str){
+		$rs = $this->query($str);
+		return isset($rs[0])?$rs[0]:'';
+	}
+	
+	public function getLastId(){
+		return mysql_insert_id($this->link);
+	}
+	
+	public function execute($str) {
+		if ( $this->resource ) {
+			$this->free();
+		}
+		$result = mysql_query($str, $this->link) ;
+		if ( false === $result) {
+			$this->error();
+			return false;
+		} else {
+			return mysql_affected_rows($this->link);
+		}
+	}
+
+	public function startTrans() {
+		mysql_query('START TRANSACTION', $this->link);
+		return ;
+	}
+
+	public function commit() {
+		$result = mysql_query('COMMIT', $this->link);
+		if(!$result){
+			$this->error();
+		}
+		return true;
+	}
+
+	public function rollback() {
+		$result = mysql_query('ROLLBACK', $this->link);
+		if(!$result){
+			$this->error();
+			return false;
+		}
+		return true;
+	}
+
+	private function getAll() {
+		$result = array();
+		while( true==( is_resource($this->resource) && 
+				$row=mysql_fetch_assoc($this->resource)) ){
+			$result[] = $row;
+		}
+		if(!empty($result)){
+			mysql_data_seek($this->resource, 0);
+		}
+		return $result;
+	}
+	
+	public function escapeString($str) {
+		if($this->link) {
+			return mysql_real_escape_string($str,$this->link);
+		}else{
+			return mysql_escape_string($str);
+		}
+	}
+
+	public function close() {
+		if ($this->link){
+			mysql_close($this->link);
+		}
+		$this->link = null;
+	}
+	
+	private function error(){
+		throw new LmlDbException(mysql_error($this->link));
+	}
+
+}
+class LmlDbException extends LmlException{}
