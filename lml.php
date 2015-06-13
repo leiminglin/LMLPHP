7fe54646d5aab521cde72ab71cc4bb58607fe594
may <leiminglin@126.com>
2015-06-13 21:30:47 +0800

    modify lml.php url

--- lml.php
+++ lml.php
@@ -15,7 +15,7 @@
 /**
 function getRemoteLmlPhp(){
 	$cache_filename = 'lml.min.php';
-	$remotelib = 'http://pro8091d8.pic12.websiteonline.cn/upload/lmlphp-release-2014-08-27-v1.txt';
+	$remotelib = 'http://git.oschina.net/leiminglin/LMLPHP/raw/master/lml.min.php';
 	if( file_exists( $cache_filename ) ) {
 		$cachemtime = filemtime($cache_filename);
 		if( $cachemtime + 86400 > time() ){
