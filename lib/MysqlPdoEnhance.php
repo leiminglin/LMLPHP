3d9a6823d016b4e88f0d272d49d53997f8134b3e
may <leiminglin@126.com>
2015-11-17 23:47:08 +0800

    add mysql pdo enhance class

---
+++ lib/MysqlPdoEnhance.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * LMLPHP Framework
+ * Copyright (c) 2014 http://lmlphp.com All rights reserved.
+ * Licensed ( http://mit-license.org/ )
+ * Author: leiminglin <leiminglin@126.com>
+ *
+ * A fully object-oriented PHP framework.
+ * Keep it light, magnificent, lovely.
+ *
+ */
+
+interface MysqlPdoInterface{
+
+	public static function getInstance($config);
+
+	public function query($sql, $params = array());
+
+	public function getOne($sql, $params = array());
+
+	public function getLastId();
+
+	public function insert($table, $arrData);
+
+	public function update($table, $arrData, $where = '');
+
+	public function delete($table, $where='', $params=array());
+}
+
+class MysqlPdoEnhance implements MysqlPdoInterface
+{
+	private static $config;
+	private static $instance;
+	private $db;
+
+	private function __construct() {
+		$dsn = 'mysql:host='.self::$config['hostname'].';dbname='.self::$config['database'];
+		$username = self::$config['username'];
+		$password = self::$config['password'];
+		$options = array(
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+		);
+		$this->db = new PDO($dsn, $username, $password, $options);
+		if ($this->db->getAttribute(PDO::ATTR_DRIVER_NAME) != 'mysql') {
+			die("MySQL support not be enabled");
+		}
+	}
+
+	public static function getInstance($config){
+		self::$config = $config;
+		if(self::$instance && self::$instance instanceof self){
+			return self::$instance;
+		}
+		return new self();
+	}
+
+	public function query($sql, $params = array()){
+		$stmt = $this->db->prepare($sql, array(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true));
+
+		if($params){
+			foreach ($params as $k => $v){
+				if(is_array($v)){
+					$value = $v['value'];
+					$type = isset($v['type']) ? $v['type'] : false;
+					$length = isset($v['length']) ? $v['length'] : false;
+					if($type && $length){
+						$stmt->bindValue($k, $value, $type, $length);
+					}elseif($type){
+						$stmt->bindValue($k, $value, $type);
+					}else{
+						$stmt->bindValue($k, $value);
+					}
+				}else{
+					$stmt->bindValue($k, $v);
+				}
+			}
+		}
+
+		$stmt->execute();
+		if(preg_match('/^update|^insert/i', trim($sql))){
+			return $stmt->rowCount();
+		}else{
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+	}
+
+	public function getOne($sql, $params = array()){
+		$result = $this->query($sql, $params);
+		return $result[0];
+	}
+
+	public function getLastId(){
+		return $this->db->lastInsertId();
+	}
+
+	public function insert($table, $data){
+		$keys = '';
+		$place_values = '';
+		$params = array();
+		foreach ($data as $k => $v){
+			$keys .= '`'.$k.'`,';
+			$place_values .= ":".$k.",";
+			$params[':'.$k] = $v;
+		}
+		$keys = rtrim($keys, ',');
+		$place_values = rtrim($place_values, ',');
+		$sql = "INSERT INTO $table ($keys) VALUES ($place_values)";
+		return $this->query($sql, $params);
+	}
+
+	public function update($table, $data, $where = ''){
+		$sql = 'UPDATE '.$table.' SET ';
+		$params = array();
+		foreach ($data as $k=>$v){
+			$sql .= '`'.$k.'`=:'.$k.',';
+			$params[':'.$k] = $v;
+		}
+		$sql = rtrim($sql, ',');
+		if($where){
+			$sql .= ' WHERE '.$where;
+		}
+		return $this->query($sql, $params);
+	}
+
+	public function delete($table, $where='', $params=array()){
+		$sql = 'DELETE FROM '.$table.' ';
+		if($where){
+			$sql .= ' WHERE '.$where;
+		}
+		return $this->query($sql, $params);
+	}
+
+}
